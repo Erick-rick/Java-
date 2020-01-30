9e2d5dec43ee9de8e34ffc7256d74ae79110c5f0
Erick-rick <42187451+Erick-rick@users.noreply.github.com>
2020-01-30 17:48:44 -0300

    .

---
+++ Help Desk/src/com/helpdesk/ClienteDAO.java
@@ -0,0 +1,23 @@
+package com.helpdesk;
+
+public class ClienteDAO {
+	
+	public void create() {
+		
+		
+	}
+	
+	public void read() {
+		
+		
+	}
+	
+	
+	public void  update() {
+		
+		
+	}
+	
+	public void delete(){}
+
+}
---
+++ Mercado/src/module-info.java
@@ -0,0 +1,4 @@
+module Mercado {
+	requires java.sql;
+	requires java.desktop;
+}
